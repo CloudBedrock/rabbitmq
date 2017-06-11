136716075d5688f55d398d638875dafe27b412f9
Roman Kuznetsov <roman@kuznero.com>
2017-06-11 23:45:37 +0200

    Enable pause_minority mode instead of autoheal

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,55 +8,67 @@ ERLANG_SECRET=secret
 TRACE_QUEUE=trace
 PORT=5672
 MGMT_PORT=15672
-DELAY=30
+DELAY=10
 
-docker network create -d overlay $NET_NAME
+n=$(docker network ls --filter name=$NET_NAME | grep -v '^NETWORK' | wc -l)
+if [[ $n -eq 0 ]]; then
+  docker network create -d overlay $NET_NAME
+fi
 
-docker service create \
-  --name $SVC_NAME-1 \
-  --network $NET_NAME \
-  -p $PORT:5672 \
-  -p $MGMT_PORT:15672 \
-  -e RABBITMQ_SETUP_DELAY=$(($DELAY*3)) \
-  -e RABBITMQ_USER=$USER_NAME \
-  -e RABBITMQ_PASSWORD=$PASSWORD \
-  -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-2 rabbit@$SVC_NAME-3" \
-  -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-1" \
-  -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
-  -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
-  -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
-  kuznero/rabbitmq:management-cluster
+n=$(docker service ls --filter name=$SVC_NAME-1 | grep -v '^ID' | wc -l)
+if [[ $n -eq 0 ]]; then
+  docker service create \
+    --name $SVC_NAME-1 \
+    --network $NET_NAME \
+    -p $PORT:5672 \
+    -p $MGMT_PORT:15672 \
+    -e RABBITMQ_SETUP_DELAY=$(($DELAY*3)) \
+    -e RABBITMQ_USER=$USER_NAME \
+    -e RABBITMQ_PASSWORD=$PASSWORD \
+    -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-2 rabbit@$SVC_NAME-3" \
+    -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-1" \
+    -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
+    -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
+    -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
+    kuznero/rabbitmq:management-cluster
+fi
 
-docker service create \
-  --name $SVC_NAME-2 \
-  --network $NET_NAME \
-  -p $(($PORT+1)):5672 \
-  -p $(($MGMT_PORT+1)):15672 \
-  -e RABBITMQ_SETUP_DELAY=$(($DELAY*2)) \
-  -e RABBITMQ_USER=$USER_NAME \
-  -e RABBITMQ_PASSWORD=$PASSWORD \
-  -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-1 rabbit@$SVC_NAME-3" \
-  -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-2" \
-  -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
-  -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
-  -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
-  kuznero/rabbitmq:management-cluster
+n=$(docker service ls --filter name=$SVC_NAME-2 | grep -v '^ID' | wc -l)
+if [[ $n -eq 0 ]]; then
+  docker service create \
+    --name $SVC_NAME-2 \
+    --network $NET_NAME \
+    -p $(($PORT+1)):5672 \
+    -p $(($MGMT_PORT+1)):15672 \
+    -e RABBITMQ_SETUP_DELAY=$(($DELAY*2)) \
+    -e RABBITMQ_USER=$USER_NAME \
+    -e RABBITMQ_PASSWORD=$PASSWORD \
+    -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-1 rabbit@$SVC_NAME-3" \
+    -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-2" \
+    -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
+    -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
+    -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
+    kuznero/rabbitmq:management-cluster
+fi
 
-docker service create \
-  --name $SVC_NAME-3 \
-  --network $NET_NAME \
-  -p $(($PORT+2)):5672 \
-  -p $(($MGMT_PORT+2)):15672 \
-  -e RABBITMQ_SETUP_DELAY=$DELAY \
-  -e RABBITMQ_USER=$USER_NAME \
-  -e RABBITMQ_PASSWORD=$PASSWORD \
-  -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-1 rabbit@$SVC_NAME-2" \
-  -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-3" \
-  -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
-  -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
-  -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
-  kuznero/rabbitmq:management-cluster
+n=$(docker service ls --filter name=$SVC_NAME-3 | grep -v '^ID' | wc -l)
+if [[ $n -eq 0 ]]; then
+  docker service create \
+    --name $SVC_NAME-3 \
+    --network $NET_NAME \
+    -p $(($PORT+2)):5672 \
+    -p $(($MGMT_PORT+2)):15672 \
+    -e RABBITMQ_SETUP_DELAY=$DELAY \
+    -e RABBITMQ_USER=$USER_NAME \
+    -e RABBITMQ_PASSWORD=$PASSWORD \
+    -e RABBITMQ_CLUSTER_NODES="rabbit@$SVC_NAME-1 rabbit@$SVC_NAME-2" \
+    -e RABBITMQ_NODENAME="rabbit@$SVC_NAME-3" \
+    -e RABBITMQ_ERLANG_COOKIE=$ERLANG_SECRET \
+    -e RABBITMQ_FIREHOSE_QUEUENAME=$TRACE_QUEUE \
+    -e RABBITMQ_FIREHOSE_ROUTINGKEY=publish.# \
+    kuznero/rabbitmq:management-cluster
+fi
 
-echo -n "Waiting for nodes to establish cluster ... "
-sleep $(($DELAY*3))
-echo "done."
+# echo -n "Waiting for nodes to establish cluster ... "
+# sleep $(($DELAY*3))
+# echo "done."
